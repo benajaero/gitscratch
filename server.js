f6fbc4506d120ab8f8189c6be74eee72064fb8ab
Benny <blaze349@protonmail.com>
2017-04-03 07:08:32 +0000

    Created server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const ejs = require("ejs")
+const path = require('path')
+const bodyParser = require("body-parser")
+const logger = require("morgan")
+const cookieParser = require("cookie-parser")
+
+var app = express()
+
+app.set("views", path.join(__dirname, 'views'))
+app.set("view engine", "ejs")
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('index', {})
+})
+
+app.get('/repo', (req, res) => {
+    res.render('repo', {})
+})
+
+app.listen(8080, () => {
+    console.log("Meme magic on port 8080")
+})
