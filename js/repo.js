60d46dd914700d858737b6087d785d11b9adea97
Henry Gruett <henrikgruett@gmail.com>
2017-04-02 22:49:10 -0500

    Update repo.js

--- js/repo.js
+++ js/repo.js
@@ -1,1 +1,8 @@
+function(){
+  document.getElementById("newrepo").className = "hide";
+  document.getElementById("importrepo").className = "hide";
+}
 
+//Still need to add when radio button change if radio= hide and show
+
+//Also need to add forum req
