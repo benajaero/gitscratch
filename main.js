c58f39bd5ac425b2f373b4d440d8e8631e48c324
Benny <blaze349@protonmail.com>
2017-04-14 20:09:32 +0000

    Created window, removed errors in package.json

---
+++ main.js
@@ -0,0 +1,32 @@
+//dependencies
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+//creating a new window
+let window 
+
+function makeWin() {
+    window = new BrowserWindow({width: 1000,height: 800})
+    
+    /** 
+    * Each window is a html renderer powered by chromium
+    * This is how you choose what html file the window should load. 
+    * I suppose it could also load html files over the web
+    * */
+ 
+
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, 'base.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+    
+    window.on('close', () => {
+        window = null
+    })
+}
+
+
+app.on('ready', makeWin)
+
